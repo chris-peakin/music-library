38166a25f5e5171a3fd718865e3753bc51cb6bd4
Chris Peakin <christopher.peakin@googlemail.com>
2022-02-19 13:43:45 +0000

    updated artist creation

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,20 +1,12 @@
-const express = require('express');
-//const app = require('../app');
-const artist = express();
 const getDb = require('../services/db');
-
-
-artist.use(express.json());
 
 exports.create = async (req, res) => {
     const db = await getDb();
     const { name, genre } = req.body;
   
     try {
-      await db.query(`INSERT INTO Artist (name, genre) VALUES (?, ?)`, [
-          name,
-          genre,
-      ]);
+      await db.query(`
+      INSERT INTO Artist (name, genre) VALUES ('${name}', '${genre}')`);
   
       res.sendStatus(201);
     } catch (err) {
@@ -24,19 +16,3 @@ exports.create = async (req, res) => {
   
     db.close();
   };
-
-// exports.create = async (req, res) => {
-//     const db = await getDb();
-//     const {name, genre} = req.body
-//     try{
-//         console.log(name, genre);
-//         console.log(`INSERT INTO Artist (name, genre) VALUES ('Tame Impala', 'rock')`);
-//         await db.query(`INSERT INTO Artist (name, genre) VALUES ('Tame Impala', 'rock')`);
-//         res.sendStatus(201);
-//     } catch (err){
-//         console.log(err)
-//         res.sendStatus(500).json(err);
-//     }
-//     db.close();
-// };
-
