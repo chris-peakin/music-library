019dc6968d8aec2cebb2b656543ba71112ff140a
Chris Peakin <christopher.peakin@googlemail.com>
2022-02-26 17:24:06 +0000

    wip

--- src/controllers/album.js
+++ src/controllers/album.js
@@ -19,3 +19,78 @@ exports.create = async (req, res) => {
 
     db.close();
 }
+
+exports.read = async(_, res) => {
+    const db = await getDb();
+
+    try{
+        const [albums] = await db.query('SELECT * FROM Album');
+        res.status(200).json(albums);
+    } catch (err){
+        console.log(err);
+        res.status(500).json(err);
+    }
+    db.close();
+}
+
+exports.readSingle = async (req, res) =>{
+    const db = await getDb();
+    const {albumId} = req.params;
+    const [[album]] = await db.query('SELECT * FROM Album WHERE id = ?', [
+        albumId,
+    ]);
+
+    if (!album){
+        res.sendStatus(404);
+    } else {
+        res.status(200).json(album);
+    }
+
+    db.close();
+}
+
+exports.update = async (req, res) =>{
+    const db = await getDb();
+    const {albumId} = req.params;
+    const data = req.body;
+
+    try{
+        const [
+            {affectedRows},
+        ] = await db.query('UPDATE Album SET ? WHERE id = ?', [data, albumId]);
+
+        if (!affectedRows){
+            res.sendStatus(404);
+        } else {
+            res.status(200).send();
+        }
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+    db.close();
+};
+
+
+exports.delete = async (req, res) =>{
+    const db = await getDb();
+    const {albumId} = req.params;
+    const [[album]] = await db.query('SELECT * FROM Album Where id = ?', [
+        albumId,
+    ]);
+
+    if(!album){
+        res.sendStatus(404);
+    }
+
+    try {
+        await db.query('DELETE FROM Album WHERE id = ?', [
+            albumId
+        ])
+        res.sendStatus(200);
+    } catch (err){
+        console.log(err)
+        res.sendStatus(404).json(err);
+    }
+    db.close();
+}
--- src/routes/album.js
+++ src/routes/album.js
@@ -6,4 +6,12 @@ const router = express.Router();
 
 router.post('/artist/:artistId', albumController.create);
 
+router.get('/album', albumController.readSingle);
+
+router.get('/album/:albumId', albumController.read);
+
+router.patch('/album/:albumId', albumController.update);
+
+router.delete('/album/:albumId', albumController.delete);
+
 module.exports = router;
